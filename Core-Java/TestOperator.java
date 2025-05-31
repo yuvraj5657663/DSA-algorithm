c1794897965ecbb94619cdeeba50779cbe6290d2
yuvraj kumar <yuvrajkumar@gmail.com>
2025-05-31 12:10:17 +0530

    test-Operator

---
+++ Core-Java/TestOperator.java
@@ -0,0 +1,10 @@
+
+
+public class TestOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        // a(short)(a+1);
+        System.out.println(a++);
+        System.out.println(a--);
+    }
+}
